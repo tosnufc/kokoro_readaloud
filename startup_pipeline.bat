c7613f3a65076786d54a5654c540653faa6ab462
tosnufc <tosnufc@gmail.com>
2025-06-03 20:06:42 +0700

    start minimized

--- startup_pipeline.bat
+++ startup_pipeline.bat
@@ -1,2 +1,2 @@
 @echo off
-start /B d:/dev/kokoro_readaloud/.venv/Scripts/python.exe d:/dev/kokoro_readaloud/pipeline_server.py
+start /min d:/dev/kokoro_readaloud/.venv/Scripts/python.exe d:/dev/kokoro_readaloud/pipeline_server.py
